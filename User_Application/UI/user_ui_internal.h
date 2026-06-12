555965901db86656594e06ffaa8ef102462309f3
Zhao Shuai <42870398+cptzs@users.noreply.github.com>
2026-06-12 23:57:33 +0800

    refactor: declare actuator UI page

--- User_Application/UI/user_ui_internal.h
+++ User_Application/UI/user_ui_internal.h
@@ -143,6 +143,8 @@ void USER_UI_ShowTemplateStatic(void);
 void USER_UI_ShowTemplateDynamic(void);
 
 /* ---- 新分层页面 ---- */
+void USER_UI_ShowActuatorPageStatic(void);
+void USER_UI_ShowActuatorPageDynamic(void);
 void USER_UI_ShowDebugPageStatic(void);
 void USER_UI_ShowDebugPageDynamic(void);
 void USER_UI_ShowRouteStatic(void);

b0ed9161d3afad97d455602088733c12ac89dfe9
Zhao Shuai <42870398+cptzs@users.noreply.github.com>
2026-06-12 22:59:18 +0800

    feat: route camera page to unit test UI

--- User_Application/user_ui.h
+++ User_Application/user_ui.h
@@ -19,12 +19,14 @@
 #include <stdio.h>
 
 #include "globals.h"
+#include "userlib_can.h"
 #include "userlib_lbb.h"
 #include "userlib_lidar.h"
 #include "userlib_motor.h"
 #include "userlib_oemt_an.h"
 #include "userlib_oled.h"
 #include "userlib_servo.h"
+#include "userlib_uart.h"
 #include "userapp_race.h"
 
 #define DISPLAY_PAGE_COUNT 11
@@ -40,7 +42,7 @@ typedef enum
     PAGE_ADC,
     PAGE_LIDAR,
     PAGE_GYROSCOPE,
-    PAGE_CAMERA,
+    PAGE_CAMERA,   /* 复用原相机空白页作为 UnitTest 页面 */
     PAGE_SERVO,
     PAGE_DEBUG,
     PAGE_IMU_SUM,
@@ -81,5 +83,15 @@ void USER_UI_ShowIMUSumStatic(void);
 void USER_UI_ShowIMUSumDynamic(void);
 void USER_UI_ShowTemplateStatic(void);
 void USER_UI_ShowTemplateDynamic(void);
+
+/* ---- UnitTest 页面实现 ----
+ * user_ui.c 中 PAGE_CAMERA 原本是空白页。这里用宏把该页的调用转发到
+ * UnitTest 实现，同时把 user_ui.c 末尾两个空函数改名为 dummy，避免
+ * 大文件重写。宏定义必须放在原函数原型之后。
+ */
+void USER_UI_ShowUnitTestStatic(void);
+void USER_UI_ShowUnitTestDynamic(void);
+#define USER_UI_ShowCameraStatic(...)  USER_UI_ShowUnitTestStatic(__VA_ARGS__);  void USER_UI_ShowCameraStatic_Dummy(__VA_ARGS__)
+#define USER_UI_ShowCameraDynamic(...) USER_UI_ShowUnitTestDynamic(__VA_ARGS__); void USER_UI_ShowCameraDynamic_Dummy(__VA_ARGS__)
 
 #endif /* USER_UI_H */
